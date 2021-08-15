244f044ca8953865bf3fadb0086c49fd6dad6342
Cykk3828 <74847929+Cykk3828@users.noreply.github.com>
2021-08-15 15:31:30 +0530

    Add files via upload

---
+++ CarRacingStage1.5/js/Player.js
@@ -0,0 +1,46 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+  getCarsend(){
+database.ref('carsend').on("value",(data)=>{
+  this.rank = data.val()
+})
+
+  }             
+   static updatecarsend(rank){
+     database.ref('/').update({
+       carsend: rank
+     })
+   } 
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
